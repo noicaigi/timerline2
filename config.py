75abeb7097b868270991c071427dd411f74a8603
noicaigi <116483916+noicaigi@users.noreply.github.com>
2025-06-27 22:01:33 +0700

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.getenv('API_ID'))
-API_HASH = str(os.getenv('API_HASH'))
-BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+API_ID = int(os.getenv('27666286'))
+API_HASH = str(os.getenv('bb4c9fe90f5ea9b6d9fae18f2ea6c7fa'))
+BOT_TOKEN = str(os.getenv('7223341399:AAFrbwFlc3mmEZFMDm6C7uzkZ1SQheQMAIA'))
 SESSIONS_DIRECTORY = os.getenv('SESSIONS_DIRECTORY')
 DATABASE_URL = os.getenv('DATABASE_URL')
 DATABASE_ECHO = bool(os.getenv('DATABASE_ECHO'))
